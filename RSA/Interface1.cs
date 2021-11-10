4eba8ad7b0cad21ceefc03e233649305eb1a17a0
Zroeth <31348558+Zroeth@users.noreply.github.com>
2021-11-09 22:51:47 -0600

    finx21

---
+++ RSA/Interface1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace RSA
+{
+    interface Interface1
+    {
+        List<string> generarLlave(int p, int q);
+        List<byte> RSACifrado(FileStream archivo, int n, int e);
+        List<byte> descifrar(FileStream archivo, int n, int d);
+    }
+}
